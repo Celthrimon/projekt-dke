8771821a814abf7c6352e55c8312f387e498349d
Celthirmon <mario.lanz614@gmail.com>
2021-10-18 14:32:48 +0200

    added project directories and update gitignore for maven

---
+++ FollowingService/src/main/java/at/jku/followingservice/FollowingServiceApplication.java
@@ -0,0 +1,13 @@
+package at.jku.followingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FollowingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FollowingServiceApplication.class, args);
+    }
+
+}
---
+++ LogService/src/test/java/at/jku/logservice/LogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package at.jku.logservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
