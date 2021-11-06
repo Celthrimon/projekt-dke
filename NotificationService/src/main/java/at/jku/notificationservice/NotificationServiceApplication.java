681d88bcd97878c9bbf4b12d85c4092d4dbf6374
ineb01 <benjamin.bachmayr@gmil.com>
2021-11-06 13:17:40 +0100

    Docker all projects edited

--- NotificationService/src/main/java/at/jku/notificationservice/NotificationServiceApplication.java
+++ NotificationService/src/main/java/at/jku/notificationservice/NotificationServiceApplication.java
@@ -2,9 +2,12 @@ package at.jku.notificationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class NotificationServiceApplication {
+
 
     public static void main(String[] args) {
         SpringApplication.run(NotificationServiceApplication.class, args);
