8f17d013de5dbd1690d2cd72d25cb13cc8bade27
Celthirmon <mario.lanz614@gmail.com>
2022-01-20 18:17:09 +0100

    added login, register and navbar designs

--- frontend/src/Components/App.js
+++ frontend/src/Components/App.js
@@ -1,8 +1,23 @@
 import Userpanel from './Userpanel'
+import PrimarySearchAppBar from './navbar';
+import Login from './pages/Login';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Register from './pages/Register';
+
 function App() {
   return (
     <div className="App">
-      <Userpanel user={'Mario'} />
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Login} />
+          <Route path="/register" exact component={Register} />
+          <Route path="/view">
+            <PrimarySearchAppBar></PrimarySearchAppBar>
+            <br></br>
+            <Userpanel user={'Mario'} />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
